bb8d70add4f71fec075180d2e187c7191cec1fe2
Arthur <arthurbrmn@gmail.com>
2026-09-08 10:52:09 -0300

    Atividade 2

--- Atividade 001/atividade 01_exercicios poo.py
+++ Atividade 001/atividade 01_exercicios poo.py
@@ -10,11 +10,45 @@ class Veiculo:
         self.ligado = False
 
     def acelerar(self, quantidade):
-        self.velocidade_atual += quantidade
+        if self.ligado == True:
+            self.velocidade_atual += quantidade
 
-    def frear(self, quantidade_f):
-        if self.velocidade_atual <= 0:
+
+    def frear(self,quantidade_f):
+        if self.velocidade_atual > 0:
+            self.velocidade_atual -= quantidade_f
+        elif self.velocidade_atual <= 0:
             self.velocidade_atual = 0
 
+# Se ligado == False vai virar True
     def ligar (self):
-        self.ligado = True
+        if self.ligado == False:
+            self.ligado = True
+
+# Se ligado == True vai virar False e zerar a velocidade
+    def desligar (self):
+        if self.ligado == True:
+            self.ligado = False
+            self.velocidade_atual = 0
+
+# Objeto recebe a classe
+veiculo1 = Veiculo()
+veiculo1.ligar()
+veiculo1.acelerar(50)
+veiculo1. frear(25)
+print(veiculo1.velocidade_atual, veiculo1.ligado)
+
+# 2. Classe ContaBancaria:
+
+class ContaBancaria:
+    def __init__(self):
+        self.titular =''
+        self.numero_conta = 0
+        self.saldo = 0.0
+
+    def depositar(self, valor):
+        valor += self.saldo
+
+    def sacar (self, valor):
+        if valor <= self.saldo:
+            valor -= self.saldo
